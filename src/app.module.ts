75b135c4df22268e1f17bfa72690b667da52dadb
Daniele Usai <danieleusai925@yahoo.it>
2023-01-02 14:32:49 +0100

    global exception handling added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,14 @@ import configuration from './configuration';
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { UserModule } from './user/user.module';
 import { WatchlistModule } from './watchlist/watchlist.module';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @Module({
   imports: [
@@ -25,3 +33,38 @@ import { WatchlistModule } from './watchlist/watchlist.module';
   ],
 })
 export class AppModule {}
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: InternalServerErrorException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    /**
+     * @description Exception json response
+     * @param message
+     */
+    const responseMessage = (type: string, message: string) => {
+      response.status(status).json({
+        statusCode: status,
+        path: request.url,
+        errorType: type,
+        errorMessage: message,
+      });
+    };
+
+    // Throw an exceptions for either
+    // MongoError, ValidationError, TypeError, CastError and Error
+    if (exception.message) {
+      responseMessage('Error', exception.message);
+    } else {
+      responseMessage(exception.name, exception.message);
+    }
+  }
+}
